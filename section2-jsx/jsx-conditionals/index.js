0c9333c30d5e15ab7289e01e2d7a61ad7656ea49
simpledan95 <daniel.lee@simpleviewinc.com>
2023-01-24 12:08:35 -0500

    finished section 2 jsx

---
+++ section2-jsx/jsx-conditionals/index.js
@@ -0,0 +1,29 @@
+function getNum() {
+  return Math.floor(Math.random() * 10) + 1
+}
+
+class NumPicker extends React.Component {
+  render () {
+    const num = getNum()
+    let msg;
+    
+    if (num === 7) {
+      msg = 
+        <div>
+          <h2>CONGRATS YOU WIN!</h2>
+          <img src="https://media.tenor.com/uHrMA6HIKrwAAAAd/bh187-woo.gif"/>
+        </div>
+    } else {
+      msg = <p>Sorry You Lose!</p>
+    }
+
+    return (
+      <div>
+        <h1>Your number is {num}</h1>
+        {msg}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<NumPicker/>, document.getElementById('root'))
